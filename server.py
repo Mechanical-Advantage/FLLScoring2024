533464f5e0b8a5f657a676895440dc9e18fa34c7
Aum Patel <aumpatel130806@gmail.com>
2022-08-29 21:04:57 -0400

    Make database and upload data to server
    Co-authored-by: Jonah <jonahb55@users.noreply.github.com>
    Co-authored-by: aravarora05@gmail.com <aravarora05@gmail.com>
    Co-authored-by: Neel <harnwalN@users.noreply.github.com>

--- server.py
+++ server.py
@@ -16,7 +16,19 @@ class StringMaker(object):
    @cherrypy.expose
    def greeting(self, name):
        return "Hello " + name + "!"
-		
+	
+   @cherrypy.expose
+   def saveMatch(self, team, match, referee, table, score, GPScore, teamInitials, precisionTokens):
+        print("Team: " + team + "\nMatch: " + match + "\nReferee:  " +  referee + "\nTable: " + table + "\nScore " + score + "\nGPScore " + GPScore + "\nTeam Initials: " + teamInitials + "\nPrecision Tokens: " + precisionTokens)
+        
+        conn_global = sql.connect(db_global)
+        cur_global = conn_global.cursor()
+        cur_global.execute(
+            "INSERT INTO matches(team, match, referee, table, score, GPScore, teamInitials, precisionTokens) VALUES (team, match, referee, table, score, GPScore, teamInitials, precisionTokens)")
+        conn_global.commit()
+        conn_global.close()
+        return
+        
 if __name__ == '__main__':
     cherrypy.config.update(
             {"server.socket_port":8080, "server.socket_host": "0.0.0.0"})
@@ -26,3 +38,4 @@ if __name__ == '__main__':
             "tools.staticdir.dir": os.getcwd() + "\\static"
         }
     })
+
