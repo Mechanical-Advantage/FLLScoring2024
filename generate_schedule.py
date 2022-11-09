bfe0de57941d219e419c13b14237d1dc36b91d59
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-11-09 17:03:12 -0500

    Add staggered match scheduler

---
+++ generate_schedule.py
@@ -0,0 +1,274 @@
+import sqlite3 as sql
+import itertools
+import copy
+import math
+from datetime import datetime
+import config
+import schedule_excel_writer as excel_writer
+
+# Setup db connection
+conn = sql.connect(config.db_path)
+cur = conn.cursor()
+
+
+# Function for rendering time w/o padding
+def convert_time(timestamp, include_p=False):
+    hour = datetime.fromtimestamp(timestamp).strftime("%I")
+    while hour[:1] == "0":
+        hour = hour[1:]
+    if include_p:
+        minute_string = ":%M %p"
+    else:
+        minute_string = ":%M"
+    return(hour + datetime.fromtimestamp(timestamp).strftime(minute_string))
+
+
+# Generate team objects
+teams = {}
+teams_raw = cur.execute(
+    "SELECT teamNumber FROM teams ORDER BY teamNumber ASC").fetchall()
+for teamrow in teams_raw:
+    teams[int(teamrow[0])] = {"match_count": 0, "last_match": -999, "previous_opponents": [],
+                              "previous_tables": {}, "blackout_start": 0, "blackout_end": 0}
+    for table in range(len(config.schedule_tables_long)):
+        teams[int(teamrow[0])]["previous_tables"][table] = 0
+
+# Create judging sessions
+judging_sessions = []
+judging_session_count = math.ceil(len(teams)/len(config.schedule_judging_rooms))
+judging_session_length = config.schedule_judging_inlength + config.schedule_judging_outlength
+for i in range(judging_session_count):
+    start_time = config.schedule_starttime + (judging_session_length * i)
+    judging_sessions.append({"teams": [], "start_time": start_time,
+                             "end_time": start_time + config.schedule_judging_inlength})
+
+# Assign judging sessions
+i = 0
+for teamnumber in teams.keys():
+    judging_sessions[i]["teams"].append(teamnumber)
+    teams[teamnumber]["blackout_start"] = judging_sessions[i]["start_time"] - \
+        config.schedule_judging_teamgrace
+    teams[teamnumber]["blackout_end"] = judging_sessions[i]["end_time"] + config.schedule_judging_teamgrace
+    if len(judging_sessions[i]["teams"]) >= len(config.schedule_judging_rooms):
+        i += 1
+
+# Print judging sessions
+print("\nJudging sessions:")
+[print(x) for x in judging_sessions]
+print("Ends at", convert_time(
+    judging_sessions[len(judging_sessions)-1]["end_time"], True))
+
+
+# Generate possible arrangements of teams
+def get_arrangements(pair_count):
+    result = []
+    for pair_arrangement in list(itertools.permutations(range(pair_count))):
+        for reverses in list(itertools.product([0, 1], repeat=pair_count)):
+            temp = {}
+            temp["pairs"] = pair_arrangement
+            temp["reverses"] = reverses
+            result.append(temp)
+    return(result)
+
+
+# Get team -1 with correct data
+def getFillerTeam():
+    output = {"number": -1, "match_count": 0, "last_match": 0, "previous_opponents": [],
+              "previous_tables": {}, "blackout_start": None, "blackout_end": None}
+    for table in range(int(len(config.schedule_tables_long))):
+        output["previous_tables"][table] = 0
+    return(output)
+
+
+# Create single mach
+def create_match(start_time, end_time, match_number):
+    # Get list of possible teams
+    teams_sorted = []
+    incomplete_count = 0
+    for teamnumber in teams.keys():
+        outside_blackout = (teams[teamnumber]["blackout_start"] <= start_time and teams[teamnumber]["blackout_end"] <= start_time) or (
+            teams[teamnumber]["blackout_start"] >= end_time and teams[teamnumber]["blackout_end"] >= end_time)
+        matches_incomplete = teams[teamnumber]["match_count"] < config.schedule_match_countperteam
+        match_grace_complete = match_number - \
+            teams[teamnumber]["last_match"] > config.schedule_match_teamgrace
+        if matches_incomplete:
+            incomplete_count += 1
+        if outside_blackout and matches_incomplete and match_grace_complete:
+            temp_team = copy.deepcopy(teams[teamnumber])
+            temp_team["number"] = teamnumber
+            teams_sorted.append(temp_team)
+
+    # Sort teams
+    teams_sorted = sorted(teams_sorted, key=lambda team: (
+        team["last_match"], team["number"]))
+
+    # Add extra teams
+    while len(teams_sorted) < len(config.schedule_tables_long):
+        teams_sorted.append(getFillerTeam())
+
+    # Create pairs
+    pairs = []
+    for i in range(int(len(config.schedule_tables_long)/2)):
+        pairs.append([])
+        pairs[i].append(teams_sorted[0])
+        first_team = True
+        for team in teams_sorted:
+            if (team["number"] not in pairs[i][0]["previous_opponents"]) and (team["number"] != -1) and (not first_team):
+                pairs[i].append(team)
+                teams_sorted.remove(team)
+                break
+            first_team = False
+        if len(pairs[i]) == 1:
+            if teams_sorted[1]["number"] == -1 and incomplete_count > 1:
+                # They are the last team to schedule in this match, but others are incomplete (hold off scheduling them)
+                pairs.pop()
+                pairs.append([getFillerTeam()] * 2)
+                continue
+            else:
+                # The next team is a previous opponent, or this is the last team to be scheduled
+                team = teams_sorted[1]
+                pairs[i].append(team)
+                teams_sorted.remove(team)
+
+        teams_sorted.remove(teams_sorted[0])
+
+    # Test possible arrangements
+    arrangements = []
+    for arrangement_base in get_arrangements(int(len(config.schedule_tables_long)/2)):
+        arrangement = {"teams": [], "table_repeats": 0}
+        for pair_number in arrangement_base["pairs"]:
+            reversed = arrangement_base["reverses"][pair_number]
+
+            team1 = pairs[pair_number][reversed]
+            arrangement["teams"].append(team1["number"])
+            arrangement["table_repeats"] += team1["previous_tables"][len(
+                arrangement["teams"])-1]
+
+            team2 = pairs[pair_number][1-reversed]
+            arrangement["teams"].append(team2["number"])
+            arrangement["table_repeats"] += team2["previous_tables"][len(
+                arrangement["teams"])-1]
+        arrangements.append(arrangement)
+
+    # Find optimal arrangement
+    teams_final = sorted(arrangements, key=lambda arrangement: (
+        arrangement["table_repeats"],) + tuple(arrangement["teams"]))[0]["teams"]
+
+    # Update team data
+    table = -1
+    for teamnumber in teams_final:
+        table += 1
+        if teamnumber != -1:
+            teams[teamnumber]["match_count"] += 1
+            teams[teamnumber]["last_match"] = match_number
+            teams[teamnumber]["previous_tables"][table] += 1
+
+            if table % 2 == 0:
+                opponent = teams_final[table + 1]
+            else:
+                opponent = teams_final[table - 1]
+            if opponent not in teams[teamnumber]["previous_opponents"]:
+                teams[teamnumber]["previous_opponents"].append(opponent)
+
+    # Return result
+    return({"start_time": start_time, "end_time": end_time, "teams": teams_final})
+
+
+# Check if all teams have played enough matches
+def matches_complete():
+    complete = True
+    for data in teams.values():
+        if data["match_count"] < config.schedule_match_countperteam:
+            complete = False
+    return(complete)
+
+
+last_break = 0
+
+
+# Generate matches
+def generate_matches(break_limit=None):
+    global last_break
+    matches = []
+    match_number = 0
+    matches_cycle = 0
+    last_played = True
+    start_time = config.schedule_starttime - config.schedule_match_cycletime
+    while not matches_complete():
+        match_number += 1
+        start_time += config.schedule_match_cycletime
+        matches_cycle += 1
+        if matches_cycle > config.schedule_match_breakfrequency + config.schedule_match_breaklength:
+            matches_cycle = 1
+        if break_limit == None:
+            past_break_limit = False
+        else:
+            past_break_limit = match_number >= break_limit
+
+        if matches_cycle > config.schedule_match_breakfrequency and not past_break_limit:
+            last_break = match_number
+            last_played = False
+            matches.append({"start_time": start_time, "end_time": start_time +
+                           config.schedule_match_cycletime, "teams": [-1] * len(config.schedule_tables_long)})
+        else:
+            match_to_append = create_match(
+                start_time, start_time + config.schedule_match_cycletime, match_number)
+            matches.append(match_to_append)
+            if match_to_append["teams"] == [-1] * len(config.schedule_tables_long):
+                if last_played:
+                    matches_cycle = config.schedule_match_breakfrequency + 1
+                    last_played = False
+                else:
+                    matches_cycle -= 1
+            else:
+                last_played = True
+    return(matches)
+
+
+# Regenerate matches if break too close to end
+teams_backup = copy.deepcopy(teams)
+matches = generate_matches()
+if len(matches) - last_break <= 1:
+    teams = teams_backup
+    matches = generate_matches(
+        break_limit=last_break-config.schedule_match_breaklength+1)
+
+# Print matches
+print("\nMatches:")
+[print(x) for x in matches]
+print("Ends at", convert_time(matches[len(matches)-1]["end_time"], True))
+
+
+# Get schedule for teams
+def get_team_schedule(team_query):
+    schedule_items = []
+    for session in judging_sessions:
+        if team_query in session["teams"]:
+            room = session["teams"].index(team_query)
+            schedule_items.append({"start_time": session["start_time"], "end_time": session["end_time"],
+                                   "title": "Judging", "location": config.schedule_judging_rooms[room]})
+
+    match_number = 0
+    table_offsettime = config.schedule_match_cycletime / (len(config.schedule_tables_long) / 2)
+    for match in matches:
+        match_number += 1
+        if team_query in match["teams"]:
+            start_time = match["start_time"] + (math.floor(match["teams"].index(team_query) / 2) * table_offsettime)
+            schedule_items.append({"start_time": start_time, "end_time": start_time + table_offsettime, "title": "Match " + str(
+                match_number), "location": "Table " + config.schedule_tables_short[match["teams"].index(team_query)]})
+    return(sorted(schedule_items, key=lambda x: (x["start_time"],)))
+
+
+# Create excel file
+team_schedules = {}
+for team in [int(x[0]) for x in teams_raw]:
+    team_schedules[team] = get_team_schedule(team)
+excel_writer.create(judging_sessions=judging_sessions,
+                    matches=matches, team_schedules=team_schedules)
+
+# Team schedule generator
+# print()
+# team_query = int(input("Enter a team number: "))
+# for item in get_team_schedule(team_query):
+#     print(convert_time(item["start_time"]) + "-" + convert_time(
+#         item["end_time"]) + ") " + item["title"] + " (" + item["location"] + ")")
---
+++ schedule_excel_writer.py
@@ -0,0 +1,184 @@
+import xlsxwriter
+from datetime import datetime
+import config
+import math
+
+
+# Function for rendering time w/o padding
+def convert_time(timestamp, include_p=False):
+    hour = datetime.fromtimestamp(timestamp).strftime("%I")
+    while hour[:1] == "0":
+        hour = hour[1:]
+    if include_p:
+        minute_string = ":%M %p"
+    else:
+        minute_string = ":%M"
+    return(hour + datetime.fromtimestamp(timestamp).strftime(minute_string))
+
+
+def create(judging_sessions=None, matches=[], team_schedules={}):
+    # Initialization
+    workbook = xlsxwriter.Workbook(config.schedule_path)
+    formats = {}
+
+    # Add formats
+    formats["matches_title"] = workbook.add_format(
+        {"bold": True, "bg_color": "#00FF00"})
+    formats["matches_headers"] = workbook.add_format(
+        {"align": "center", "bold": True, "top": True, "bottom": True})
+    formats["matches_headers_time"] = workbook.add_format(
+        {"align": "center", "bold": True, "top": True, "bottom": True, "left": True})
+    formats["matches_data"] = workbook.add_format({"align": "center"})
+    formats["matches_data_time"] = workbook.add_format({"align": "center", "bold": True, "left": True})
+    formats["judging_title"] = workbook.add_format(
+        {"bold": True, "bg_color": "#00FFFF"})
+    formats["judging_headers"] = workbook.add_format(
+        {"align": "center", "valign": "vcenter", "bold": True, "top": True, "text_wrap": True})
+    formats["judging_data"] = workbook.add_format({"align": "center"})
+    formats["table_title"] = workbook.add_format(
+        {"bold": True, "bg_color": "#FFC266"})
+    formats["table_full_title"] = workbook.add_format(
+        {"align": "center", "valign": "vcenter", "bold": True, "bg_color": "#FFC266", "font_size": 30})
+    formats["table_full_headers"] = workbook.add_format(
+        {"align": "center", "valign": "vcenter", "bold": True, "top": True, "bottom": True, "text_wrap": True, "font_size": 20})
+    formats["table_full_data"] = workbook.add_format(
+        {"align": "center", "valign": "vcenter", "font_size": 20})
+    formats["schedule_title"] = workbook.add_format(
+        {"bold": True, "bg_color": "#FFFF00"})
+
+    # Create match overview setup
+    matches_sheet = workbook.add_worksheet("Match Schedule")
+    matches_sheet.merge_range(0, 0, 0, round(len(
+        config.schedule_tables_long) / 2 * 3), "ROBOT ROUNDS SCHEDULE", formats["matches_title"])
+    matches_sheet.write(1, 0, "Match #", formats["matches_headers"])
+    for i in range(int(len(config.schedule_tables_long) / 2)):
+        matches_sheet.set_column((i * 3) + 1, (i * 3) + 1, 6)
+        matches_sheet.write(1, (i * 3) + 1, "Time", formats["matches_headers_time"])
+        matches_sheet.write(
+            1, (i * 3) + 2, config.schedule_tables_long[i * 2], formats["matches_headers"])
+        matches_sheet.write(
+            1, (i * 3) + 3, config.schedule_tables_long[i * 2 + 1], formats["matches_headers"])
+
+    # Fill in match data
+    table_offsettime = config.schedule_match_cycletime / (len(config.schedule_tables_long) / 2)
+    match_number = 0
+    for match in matches:
+        match_number += 1
+        matches_sheet.write(match_number + 1, 0, match_number, formats["matches_data"])
+        for i in range(int(len(config.schedule_tables_long) / 2)):
+            start_time = match["start_time"] + i * table_offsettime
+            matches_sheet.write(match_number + 1, (i * 3) + 1, convert_time(
+                start_time), formats["matches_data_time"])
+            team_1 = match["teams"][i * 2]
+            team_1 = "" if team_1 == -1 else team_1
+            team_2 = match["teams"][i * 2 + 1]
+            team_2 = "" if team_2 == -1 else team_2
+            matches_sheet.write(
+                match_number + 1, (i * 3) + 2, team_1, formats["matches_data"])
+            matches_sheet.write(
+                match_number + 1, (i * 3) + 3, team_2, formats["matches_data"])
+
+    if judging_sessions != None:
+        # Create judging overview setup
+        judging_sheet = workbook.add_worksheet("Judging Schedule")
+        judging_sheet.set_column(0, 0, 12)
+        judging_sheet.set_column(1, len(config.schedule_tables_long), 8)
+        judging_sheet.merge_range(0, 0, 0, len(
+            config.schedule_judging_rooms), "JUDGING SESSION SCHEDULE", formats["judging_title"])
+        judging_sheet.write(1, 0, "Time", formats["judging_headers"])
+        for i in range(len(config.schedule_judging_rooms)):
+            judging_sheet.write(
+                1, i + 1, config.schedule_judging_rooms[i], formats["judging_headers"])
+
+        # Fill in judging data
+        i = -1
+        for session in judging_sessions:
+            i += 1
+            format = formats["judging_data"]
+
+            judging_sheet.write(
+                i + 2, 0, convert_time(session["start_time"]) + "-" + convert_time(session["end_time"]), format)
+            room = -1
+            for team in session["teams"]:
+                room += 1
+                if team == -1:
+                    to_write = ""
+                else:
+                    to_write = team
+                judging_sheet.write(i + 2, room + 1, to_write, format)
+
+    # Create sheets for each table
+    for table_number in range(int(len(config.schedule_tables_long))):
+        table_sheet = workbook.add_worksheet(
+            "Table " + config.schedule_tables_short[table_number] + " Schedule")
+        table_sheet.set_column(0, 2, 15)
+        table_sheet.merge_range(
+            0, 0, 0, 2, "TABLE " + config.schedule_tables_short[table_number] + " SCHEDULE", formats["table_title"])
+        table_sheet.write(1, 0, "Match", formats["matches_headers"])
+        table_sheet.write(1, 1, "Time", formats["matches_headers"])
+        table_sheet.write(1, 2, "Team", formats["matches_headers"])
+
+        table_sheet_full = workbook.add_worksheet(
+            "Table " + config.schedule_tables_short[table_number] + " Schedule (full)")
+        table_sheet_full.set_page_view()
+        table_sheet_full.center_horizontally()
+        table_sheet_full.set_column(0, 2, 27)
+        table_sheet_full.set_row(0, 35)
+        table_sheet_full.set_row(1, 35)
+        table_sheet_full.merge_range(
+            0, 0, 0, 2, "TABLE " + config.schedule_tables_short[table_number] + " SCHEDULE", formats["table_full_title"])
+        table_sheet_full.write(1, 0, "Match", formats["table_full_headers"])
+        table_sheet_full.write(1, 1, "Time", formats["table_full_headers"])
+        table_sheet_full.write(1, 2, "Team", formats["table_full_headers"])
+
+        row = 0
+        match_number = 0
+        specific_table_offset = math.floor(table_number / 2) * table_offsettime
+        for match in matches:
+            team = match["teams"][table_number]
+            match_number += 1
+            if team != -1:
+                row += 1
+                table_sheet.write(row + 1, 0, match_number,
+                                  formats["matches_data"])
+                table_sheet.write(row + 1, 1, convert_time(match["start_time"] + specific_table_offset) + "-" + convert_time(
+                    match["start_time"] + specific_table_offset + table_offsettime), formats["matches_data"])
+                table_sheet.write(row + 1, 2, team, formats["matches_data"])
+                table_sheet_full.write(
+                    row + 1, 0, match_number, formats["table_full_data"])
+                table_sheet_full.write(row + 1, 1, convert_time(match["start_time"] + specific_table_offset) + "-" + convert_time(
+                    match["start_time"] + specific_table_offset + table_offsettime), formats["table_full_data"])
+                table_sheet_full.write(
+                    row + 1, 2, team, formats["table_full_data"])
+                table_sheet_full.set_row(row + 1, 35)
+
+    # Creates spreadsheets for each team's schedules
+    all_matches = workbook.add_worksheet("All Matches")
+    all_matches_row = 0
+    for team, schedule in team_schedules.items():
+        team_sheet = workbook.add_worksheet("Team " + str(team) + " Schedule")
+        team_sheet.set_column(0, 2, 18)
+        team_sheet.merge_range(
+            0, 0, 0, 2, "TEAM " + str(team) + " SCHEDULE", formats["schedule_title"])
+        team_sheet.write(1, 0, "Time", formats["matches_headers"])
+        team_sheet.write(1, 1, "Activity", formats["matches_headers"])
+        team_sheet.write(1, 2, "Location", formats["matches_headers"])
+
+        row = 2
+        for i in schedule:
+            team_sheet.write(row, 0, convert_time(
+                i["start_time"]) + "-" + convert_time(i["end_time"]), formats["matches_data"])
+            team_sheet.write(row, 1, i["title"], formats["matches_data"])
+            team_sheet.write(row, 2, i["location"], formats["matches_data"])
+            row += 1
+
+            # Create a table of team number, match number, and table number
+            if i["title"][:5] == "Match":
+                all_matches.write(all_matches_row, 0, team)
+                all_matches.write(all_matches_row, 1, int(i["title"][6:]))
+                all_matches.write(
+                    all_matches_row, 2, config.schedule_tables_short.index(i["location"][6:]) + 1)
+                all_matches_row = all_matches_row+1
+
+    # Close workbook
+    workbook.close()
