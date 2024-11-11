2189c906953fc94a09c3142856ede4c581b9c5cc
harnwalN <neelaksh.harnwal@gmail.com>
2024-11-11 18:34:17 -0500

    Update schedule

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # File paths
 db_path = "data.db"
-schedule_path = "schedule.xlsx"
+schedule_path = "Advantage_Scrimmage_2024_Schedule.xlsx"
 
 # Names lists for schedule
 schedule_tables_long = ["Red 1", "Red 2", "Blue 1", "Blue 2",
@@ -10,10 +10,10 @@ schedule_tables_short = ["R1", "R2", "B1",
 schedule_judging_rooms = ["Room 1", "Room 2", "Room 3", "Room 4"]  # for judging overview
 
 # General config for schedule
-schedule_starttime = 1668952800  # unix time, when does the first match & judging session begin?
+schedule_starttime = 1731852000  # unix time, when does the first match & judging session begin?
 schedule_match_countperteam = 5  # how many matches should each team play?
 schedule_match_cycletime = 600  # secs, how long between the start of each match?
-schedule_match_breakfrequency = 5  # how many matches should be played between each break?
+schedule_match_breakfrequency = 6  # how many matches should be played between each break?
 schedule_match_breaklength = 1  # how many matches should each break last?
 schedule_match_teamgrace = 1  # how many match cycles must separate two matches with the same team?
 schedule_judging_inlength = 1800  # secs, how long does each judging session take?
@@ -21,6 +21,6 @@ schedule_judging_outlength = 900  # secs, how long should the break between judg
 schedule_judging_teamgrace = 900  # secs, how long before or after judging should a team be excluded from matches?
 
 # General config for playoffs
-playoffs_starttime = 1668967200
+playoffs_starttime = 1731870900
 playoffs_cycletime = 600
-playoffs_matchnumberstart = 50
+playoffs_matchnumberstart = 100
