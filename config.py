94776388e5f91f73664d3e0061e8cf1662e0dfd6
harnwalN <neelaksh.harnwal@gmail.com>
2024-11-17 00:03:30 -0500

    Replace red table with white table

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ db_path = "data.db"
 schedule_path = "Advantage_Scrimmage_2024_Schedule.xlsx"
 
 # Names lists for schedule
-schedule_tables_long = ["Red 1", "Red 2", "Blue 1", "Blue 2",
+schedule_tables_long = ["White 1", "White 2", "Blue 1", "Blue 2",
                         "Yellow 1", "Yellow 2"]  # for match overview
-schedule_tables_short = ["R1", "R2", "B1",
+schedule_tables_short = ["W1", "W2", "B1",
                          "B2", "Y1", "Y2"]  # for team schedules
 schedule_judging_rooms = ["Room 1", "Room 2", "Room 3", "Room 4"]  # for judging overview
 
@@ -21,6 +21,6 @@ schedule_judging_outlength = 900  # secs, how long should the break between judg
 schedule_judging_teamgrace = 900  # secs, how long before or after judging should a team be excluded from matches?
 
 # General config for playoffs
-playoffs_starttime = 1731870900
+playoffs_starttime = 1731828600
 playoffs_cycletime = 600
 playoffs_matchnumberstart = 100
